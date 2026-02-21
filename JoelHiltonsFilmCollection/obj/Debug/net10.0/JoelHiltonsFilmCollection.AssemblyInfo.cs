74b3bf4be02e9430b886e605b738c80c39974e5b
natesaez <167953576+natesaez@users.noreply.github.com>
2026-02-20 22:08:46 -0700

    test on new branch with new view MovieList

--- JoelHiltonsFilmCollection/obj/Debug/net10.0/JoelHiltonsFilmCollection.AssemblyInfo.cs
+++ JoelHiltonsFilmCollection/obj/Debug/net10.0/JoelHiltonsFilmCollection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoelHiltonsFilmCollection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24e85661dc3b3cbe196fe3566dd7846e09802c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aac716d7964f0c506df9b9ba545094cec4341b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoelHiltonsFilmCollection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoelHiltonsFilmCollection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
